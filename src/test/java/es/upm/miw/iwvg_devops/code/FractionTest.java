02d64ce36e45515e2a2a74d9690e9f9c9517502d
ruliworst <raulfa1503@gmail.com>
2023-09-18 20:09:55 +0200

    Fraction additional tests #6

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FractionTest {
+    Fraction firstFraction = new Fraction(1, 7);;
+    Fraction secondFraction = new Fraction(2, 5);
+
     @Test
     public void testDecimal() {
         Fraction fraction = new Fraction(4, 7);
@@ -32,5 +35,46 @@ public class FractionTest {
         fraction.setDenominator(expectedDenominator);
         assertEquals(expectedNumerator, fraction.getNumerator());
         assertEquals(expectedDenominator, fraction.getDenominator());
+    }
+
+    @Test
+    public void testIsProper() {
+        Fraction fraction = new Fraction(1, 2);
+        assertTrue(fraction.isProper());
+    }
+
+    @Test
+    public void testIsImproper() {
+        Fraction fraction = new Fraction(5, 2);
+        assertTrue(fraction.isImproper());
+    }
+
+    @Test
+    public void testIsEquivalent() {
+        assertFalse(firstFraction.isEquivalent(secondFraction));
+    }
+
+    @Test
+    public void testAdd() {
+        Fraction expectedFraction = new Fraction(19,  35);
+        Fraction resultFraction = firstFraction.add(secondFraction);
+        assertEquals(expectedFraction.getNumerator(), resultFraction.getNumerator());
+        assertEquals(expectedFraction.getDenominator(), resultFraction.getDenominator());
+    }
+
+    @Test
+    public void testMultiply() {
+        Fraction expectedFraction = new Fraction(2,  35);
+        Fraction resultFraction = firstFraction.multiply(secondFraction);
+        assertEquals(expectedFraction.getNumerator(), resultFraction.getNumerator());
+        assertEquals(expectedFraction.getDenominator(), resultFraction.getDenominator());
+    }
+
+    @Test
+    public void testDivide() {
+        Fraction expectedFraction = new Fraction(5,  14);
+        Fraction resultFraction = firstFraction.divide(secondFraction);
+        assertEquals(expectedFraction.getNumerator(), resultFraction.getNumerator());
+        assertEquals(expectedFraction.getDenominator(), resultFraction.getDenominator());
     }
 }
